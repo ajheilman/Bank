3fa01c73450a232150844526784ba8fae5214de3
Ashley Heilman <ajheilman@lcmail.lcsc.edu>
2017-07-16 15:05:49 -0700

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,31 +1,39 @@
 class Bank(object):
     customers = 0
     
-    def __init__(self, name, balance):
+    def __init__(self, name, balance, pin):
         self._name = name
         self._balance = balance
+        self._pin = pin
         Bank.customers += 1
 
     def displayCustomers(self):
         return Bank.customers
     
     def getName(self):
-        return self._name
+        return "The customer's name is: " + self._name
 
     def getBalance(self):
         return "$" + str(self._balance) + '\n'
+    
+    def getPin(self):
+        return self._name + "'s pin number is: " + str(self._pin)
+
+    def setPin(self, newPin):
+        self._pin = newPin
+        return "Your new pin number is: " + str(self._pin)
 
     def deposit(self, amount):
         if amount == 0:
             return "You can't deposit nothing!"
         else:
             self._balance += amount
-            return self._balance
+            return "You new balance is: $" + str(self._balance)
 
     def withdrawl(self, amount):
-        if not amount.isdigit():
-            return "You must enter a number!"
-        elif self._balance == 0:
+        '''if not amount.isdigit():
+            return "You must enter a number!"'''
+        if self._balance == 0:
             return "You can't withdraw any money because you have none!"
         elif amount > self._balance:
             return "You don't have that much money to withdraw!"
@@ -33,7 +41,9 @@ class Bank(object):
             return "You can't withdraw nothing!"
         else:
             self._balance -= amount
-            return self._balance
+            return "You new balance is: $" + str(self._balance)
 
     def __str__(self):
-        return "Name: " + self._name + "\nBalance: $" + str(self._balance) + '\n'
+        return "Name: " + self._name + \
+               "\nBalance: $" + str(self._balance) + \
+               "\nPin: " + str(self._pin) + '\n'
