8d105ff16ba0ade01554542d126f840aa84990e7
Ashley Heilman <ajheilman@lcmail.lcsc.edu>
2017-07-16 16:16:41 -0700

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,49 +1,20 @@
+from savingsAccount import savingsAccount
+
 class Bank(object):
     customers = 0
-    
-    def __init__(self, name, balance, pin):
-        self._name = name
-        self._balance = balance
-        self._pin = pin
-        Bank.customers += 1
 
-    def displayCustomers(self):
-        return Bank.customers
-    
-    def getName(self):
-        return "The customer's name is: " + self._name
-
-    def getBalance(self):
-        return "$" + str(self._balance) + '\n'
-    
-    def getPin(self):
-        return self._name + "'s pin number is: " + str(self._pin)
-
-    def setPin(self, newPin):
-        self._pin = newPin
-        return "Your new pin number is: " + str(self._pin)
-
-    def deposit(self, amount):
-        if amount == 0:
-            return "You can't deposit nothing!"
-        else:
-            self._balance += amount
-            return "You new balance is: $" + str(self._balance)
-
-    def withdrawl(self, amount):
-        '''if not amount.isdigit():
-            return "You must enter a number!"'''
-        if self._balance == 0:
-            return "You can't withdraw any money because you have none!"
-        elif amount > self._balance:
-            return "You don't have that much money to withdraw!"
-        elif amount == 0:
-            return "You can't withdraw nothing!"
-        else:
-            self._balance -= amount
-            return "You new balance is: $" + str(self._balance)
+    def __init__(self):
+        self._accounts = {}
 
     def __str__(self):
-        return "Name: " + self._name + \
-               "\nBalance: $" + str(self._balance) + \
-               "\nPin: " + str(self._pin) + '\n'
+        return "\n".join(map(str, self._accounts.values()))
+
+    def add(self, account):
+        self._accounts[account.getPin()] = account
+        Bank.customers += 1
+
+    def getNumOfCustomers(self):
+        if Bank.customers == 1:
+            return "There is " + str(Bank.customers) + " customer at the bank."
+        else:
+            return "There are " + str(Bank.customers) + " customers at the bank."
